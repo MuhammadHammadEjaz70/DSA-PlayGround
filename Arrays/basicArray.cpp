abb49cb3c0ef1e446691aba70201c65c6fdb4570
MuhammadHammadEjaz70 <SP19-BSE-070@cuilahore.edu.pk>
2022-07-25 22:15:04 +0500

    ...

---
+++ Arrays/basicArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[6] = {'H', 'a', 'm', 'm', 'a', 'd'};
+    for (int i = 0; i < 6; i++)
+    {
+        cout << name[i] << " ";
+    }
+    cout << endl;
+    
+    int arr[10] = {1};
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] = 1    << endl;
+    
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Arrays/helloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    // char ab = 'ab';
+    // cout<< ab<<endl;
+    // // this is not allow, only single character is allowed
+
+    string a = "1";
+    cout << a << endl;
+
+    int size= sizeof(a);
+    cout<< "size of a is : "<<size<< endl;
+}
